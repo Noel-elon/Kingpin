db2da8f064a28b5f2969230d726fb28f06fb043c
Noel-elon <noelnwaelugo@gmail.com>
2020-09-11 10:01:59 +0100

    splash

--- app/src/main/java/com/example/kingpin/ui/FormActivity.kt
+++ app/src/main/java/com/example/kingpin/ui/FormActivity.kt
@@ -8,10 +8,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.view.Window
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -42,12 +39,12 @@ class FormActivity : AppCompatActivity() {
 
 
         submit.setOnClickListener {
-//            if (firstName.text.isEmpty() || lastName.text.isEmpty() || email.text.isEmpty() || github.text.isEmpty()) {
-//                Toast.makeText(this, "Fill all spaces please", Toast.LENGTH_SHORT).show()
-//            } else {
-            showDialog(this)
+            if (firstName.text.isEmpty() || lastName.text.isEmpty() || email.text.isEmpty() || github.text.isEmpty()) {
+                Toast.makeText(this, "Fill all spaces please", Toast.LENGTH_SHORT).show()
+            } else {
+                showDialog(this)
 
-            //}
+            }
 
         }
 
@@ -67,35 +64,16 @@ class FormActivity : AppCompatActivity() {
         dialogButton.setOnClickListener {
             dialog.dismiss()
             val builder = AlertDialog.Builder(this)
-//                builder.setTitle("Androidly Alert")
-//                builder.setMessage("We have a message")
             val layout = layoutInflater.inflate(R.layout.dialog, null)
             builder.setView(layout)
-
-
-//                builder.setPositiveButton() { dialog, which ->
-//                    Toast.makeText(
-//                        applicationContext,
-//                        android.R.string.yes, Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//
-//                builder.setNegativeButton(R.id.cancelTv) { dialog, which ->
-//                    Toast.makeText(
-//                        applicationContext,
-//                        android.R.string.no, Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-
             builder.show()
         }
         dialog.show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // handle arrow click here
         if (item.itemId == android.R.id.home) {
-            finish() // close this activity and return to preview activity (if there is any)
+            finish()
         }
         return super.onOptionsItemSelected(item)
     }
